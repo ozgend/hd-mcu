506ca5e1733a9a90cc8dce25e89d1a561c87268b
denolk <den.ozgen@gmail.com>
2023-12-25 15:08:28 +0300

    refactor gpio assignments

--- pico-hd-mcu/constants.js
+++ pico-hd-mcu/constants.js
@@ -8,6 +8,7 @@ const Hardware = {
   HAS_TURN_SIGNAL_SERVICE: true,
   HAS_DIRECT_SENSOR_SERVICE: false,
   HAS_MUX_SENSOR_SERVICE: false,
+  MUX_SENSOR_CONNECTED_COUNT: 4,
   TURN_SIGNAL_BLINK_RATE: 500,
   TURN_SIGNAL_BLINK_TIMEOUT: 10000,
   TURN_SIGNAL_DIAG_RATE: 100,
@@ -21,14 +22,14 @@ const Gpio = {
   DEVICE_SENSOR_TEMP: 30,     // 35 - GP30 - ADC4 - virtual
   ADC2: 28,                   // 34 - GP28 - ADC2
   ADC1: 27,                   // 32 - GP27 - ADC1
-  DEVICE_SENSOR_VREF: 26,     // 31 - GP26 - ADC0
+  DEVICE_SENSOR_VOLTS: 26,    // 31 - GP26 - ADC0
   SIGNAL_IN_LEFT: 21,         // 27 - GP21
   SIGNAL_IN_RIGHT: 20,        // 26 - GP20
   SIGNAL_OUT_LEFT: 19,        // 25 - GP19  
   SIGNAL_OUT_RIGHT: 18,       // 24 - GP18
-  MUX_SENSOR_CS: 13,          // 17 - GP13
-  MUX_SENSOR_DATA: 12,        // 16 - GP12
-  MUX_SENSOR_CLK: 11,         // 15 - GP11
+  THERMO_SENSOR_CS: 13,       // 17 - GP13
+  THERMO_SENSOR_DATA: 12,     // 16 - GP12
+  THERMO_SENSOR_CLK: 11,      // 15 - GP11
   MUX_OUT_A: 9,               // 12 - GP9
   MUX_OUT_B: 8,               // 11 - GP8
   MUX_OUT_C: 7,               // 10 - GP7
