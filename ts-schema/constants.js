24e47464273fad24fed3b729674107bcd2dd2a08
denolk <den.ozgen@gmail.com>
2024-05-09 05:08:26 +0300

    fix missing constants

--- ts-schema/constants.js
+++ ts-schema/constants.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.EventType = exports.TurnSignalCommands = exports.ServiceCommand = exports.ServiceCode = exports.Broadcasting = exports.ServiceStatus = exports.ServiceType = exports.Seperator = exports.MaxItemSize = void 0;
+exports.Gpio = exports.Hardware = exports.EventType = exports.TurnSignalCommands = exports.ServiceCommand = exports.ServiceCode = exports.Broadcasting = exports.ServiceStatus = exports.ServiceType = exports.Seperator = exports.MaxItemSize = void 0;
 exports.MaxItemSize = 9999;
 exports.Seperator = {
     SerialCommand: "+",
@@ -52,4 +52,54 @@ exports.EventType = {
     CommandForService: "SERVICE_COMMAND",
     DataFromService: "SERVICE_DATA",
     DataFromSerial: "SERIAL_DATA",
+};
+exports.Hardware = {
+    // SERIAL_BAUD_COM: 9600,
+    // SERIAL_BAUD_BT: 9600,
+    MUX_SENSOR_CONNECTED_ITEMS: [0, 1, 2, 3, 4, 5, 6, 7],
+    MUX_SENSOR_READ_INTERVAL: 1000,
+    MUX_SENSOR_READ_BATCH_TIMEOUT: 3000,
+    TURN_SIGNAL_BTN_DEBOUNCE: 50,
+    TURN_SIGNAL_BLINK_RATE: 400,
+    TURN_SIGNAL_BLINK_TIMEOUT: 10000,
+    TURN_SIGNAL_DIAG_RATE: 100,
+    TURN_SIGNAL_DIAG_COUNT: 6,
+    // TURN_SIGNAL_INTERRUPT_WAIT: 100,
+    // ADC_BIT_RESOLUTION: 12,
+    ADC_BIT_MAX_VALUE: 4096,
+    ADC_REF_MAX_VOLTAGE: 3.3,
+    ADC_CONVERSION_FACTOR: 0.0008056640625,
+    BATTERY_VOLTAGE_R1: 33000,
+    BATTERY_VOLTAGE_R2: 7500,
+    // BATTERY_VOLTAGE_LOSS: 0.01,
+    // BATTERY_VOLTAGE_MIN: 11.0,
+    // BATTERY_VOLTAGE_MAX: 15.0,
+    SERVICE_POLL_INTERVAL: 5000,
+    HEARTBEAT_PUSH_INTERVAL: 5000,
+};
+// hardware voltage simulation
+// https://www.tinkercad.com/things/irP9OkxQxpl-brilliant-leelo/
+// for standard rpi pico
+// https://pico.pinout.xyz/
+// https://electrocredible.com/raspberry-pi-pico-w-pinout-guide-diagrams/
+exports.Gpio = {
+    // functionality: GP pin,   // name - notes/wiring
+    VEHICLE_SENSOR_TEMP: 30,
+    VEHICLE_SENSOR_SPEED: 28,
+    VEHICLE_SENSOR_RPM: 27,
+    VEHICLE_SENSOR_BATT: 26,
+    VEHICLE_SENSOR_IGN: 21,
+    VEHICLE_SENSOR_AUX: 20,
+    SIGNAL_IN_LEFT: 19,
+    SIGNAL_IN_RIGHT: 18,
+    SIGNAL_OUT_LEFT: 16,
+    SIGNAL_OUT_RIGHT: 17,
+    THERMO_SENSOR_CLK: 10,
+    THERMO_SENSOR_DATA: 12,
+    THERMO_SENSOR_CS: 13,
+    MUX_OUT_A: 9,
+    MUX_OUT_B: 8,
+    MUX_OUT_C: 7,
+    BT_SERIAL_RX: 1,
+    BT_SERIAL_TX: 0, // GP0  - D_BT_TX
 };
