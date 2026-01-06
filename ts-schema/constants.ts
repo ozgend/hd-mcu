d82289771f36ad0638f04f98646326aa813ce7d3
denolk <den.ozgen@gmail.com>
2026-01-06 12:27:26 +0300

    mcu - gpio remapping

--- ts-schema/constants.ts
+++ ts-schema/constants.ts
@@ -3,6 +3,7 @@ export const MaxItemSize = 9999;
 export const FILE_TSM_CONFIG = "data.tsm-config.json";
 export const FILE_VHI_DATA = "data.vehicle-info.json";
 export const FILE_TCM_CONFIG = "data.tcm-config.json";
+export const FILE_BT_UART = "data.bt-uart.json";
 export const FILE_BUNDLE = "bundle.js";
 
 export const Seperator = {
@@ -112,7 +113,7 @@ export const Hardware = {
   // throttle control
   THROTTLE_CHANGE_THRESHOLD: 3,
   THROTTLE_OFFSET: 52 * 2, // 26 for 10-bit, 104 for 12-bit
-  THROTTLE_ADC_MIN: 1024 + 100, // (1 << (ANALOG_READ_RESOLUTION_BITS - 2)) + THROTTLE_OFFSET
+  THROTTLE_ADC_MIN: 1024 + 102, // (1 << (ANALOG_READ_RESOLUTION_BITS - 2)) + THROTTLE_OFFSET
   THROTTLE_ADC_MAX: 4095, // (1 << ANALOG_READ_RESOLUTION_BITS) - 1
   THROTTLE_SAMPLING_COUNT: 5,
   THROTTLE_SAMPLING_INTERVAL_MS: 4,
@@ -131,27 +132,35 @@ export const Hardware = {
 // https://electrocredible.com/raspberry-pi-pico-w-pinout-guide-diagrams/
 export const Gpio = {
   // functionality: GP pin,   // name - notes/wiring
-  VBUS: 24, // GP24 - VBUS - USB power detection
-  ONBOARD_LED: 25, // GP25 - LED
-  VEHICLE_SENSOR_TEMP: 30, // GP30 - ADC4 - virtual
-  VEHICLE_SENSOR_VREF: 29, // GP29 - ADC3 - internal voltage reference
-  THROTTLE_SENSOR_AUX: 28, // GP28 - ADC2 -  secondary Hall Sensor
-  THROTTLE_SENSOR_MAIN: 27, // GP27 - ADC1 - primary Hall Sensor
-  VEHICLE_SENSOR_BATT: 26, // GP26 - ADC0 - battery voltage input
-  THROTTLE_SERVO_PWM: 15, // GP15 - PWM_SERVO - throttle servo pwm output
-  SIGNAL_IN_LEFT: 19, // GP19 - D_SIG_L_IN
-  SIGNAL_IN_RIGHT: 18, // GP18 - D_SIG_R_IN
-  SIGNAL_OUT_LEFT: 16, // GP16 - D_SIG_L_OUT
-  SIGNAL_OUT_RIGHT: 17, // GP17 - D_SIG_R_OUT
+  RESERVED_0: 0, // GP0  - RESERVED 0 TX
+  RESERVED_1: 1, // GP1  - RESERVED 1 RX
+  RESERVED_2: 2, // GP2 - RESERVED 2
+  VEHICLE_SENSOR_RPM: 3, // GP3 - D_RPM_SENSOR - pulse input from rpm sensor
+  BT_SERIAL_TX: 4, // GP4  - D_BT_TX
+  BT_SERIAL_RX: 5, // GP5  - D_BT_RX
+  VEHICLE_SENSOR_IGN: 6, // GP6  - D_IGN_SENSOR - pulse input from ignition module
+  MUX_OUT_C: 7, // GP7  - D_MUX_C
+  MUX_OUT_B: 8, // GP8  - D_MUX_B
+  MUX_OUT_A: 9, // GP9  - D_MUX_A
   THERMO_SENSOR_CLK: 10, // GP10 - D_SCK
+  VEHICLE_SENSOR_SPEED: 11, // GP11 - D_SPEED_SENSOR - pulse input from speed sensor
   THERMO_SENSOR_DATA: 12, // GP12 - D_SO
   THERMO_SENSOR_CS: 13, // GP13 - D_CS
-  MUX_OUT_A: 9, // GP9  - D_MUX_A
-  MUX_OUT_B: 8, // GP8  - D_MUX_B
-  MUX_OUT_C: 7, // GP7  - D_MUX_C
-  VEHICLE_SENSOR_IGN: 5, // GP5  - D_IGN_SENSOR - TBD - pulse input from ignition module
-  VEHICLE_SENSOR_SPEED: 4, // GP4 - D_SPEED_SENSOR - TBD - pulse input from speed sensor
-  VEHICLE_SENSOR_RPM: 3, // GP3 - D_RPM_SENSOR - TBD - pulse input from rpm sensor
-  BT_SERIAL_RX: 1, // GP1  - D_BT_RX
-  BT_SERIAL_TX: 0, // GP0  - D_BT_TX
+  RESERVED_14: 14, // GP14 - RESERVED
+  THROTTLE_SERVO_PWM: 15, // GP15 - PWM_SERVO - throttle servo pwm output
+  SIGNAL_OUT_LEFT: 16, // GP16 - D_SIG_L_OUT
+  SIGNAL_OUT_RIGHT: 17, // GP17 - D_SIG_R_OUT
+  SIGNAL_IN_RIGHT: 18, // GP18 - D_SIG_R_IN
+  SIGNAL_IN_LEFT: 19, // GP19 - D_SIG_L_IN
+  RESERVED_20: 20, // GP20 - RESERVED
+  RESERVED_21: 21, // GP21 - RESERVED
+  RESERVED_22: 22, // GP22 - RESERVED
+  ONCHIP_SMPS: 23, // GP23 - SMPS - on-chip power switch - not exposed
+  ONCHIP_VBUS: 24, // GP24 - VBUS - USB power detection - not exposed
+  ONCHIP_LED: 25, // GP25 - LED - not exposed
+  VEHICLE_SENSOR_BATT: 26, // GP26 - ADC0 - battery voltage input
+  THROTTLE_SENSOR_MAIN: 27, // GP27 - ADC1 - primary Hall Sensor
+  RESERVED_28: 28, // GP28 - ADC2 - RESERVED
+  ONCHIP_VREF: 29, // GP29 - ADC3 - internal voltage reference for vsys - not exposed
+  ONCHIP_TEMP: 30, // GP30 - ADC4 - temperature sensor - not exposed
 };
